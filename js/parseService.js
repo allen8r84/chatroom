c36169de530efff6e99c480ee957c772fb3a4525
Jon <jon@allen8r.com>
2015-02-12 20:36:33 -0700

    add service functions

--- js/parseService.js
+++ js/parseService.js
@@ -1,12 +1,11 @@
 var app = angular.module('chatroom');
 
-app.service('parseService', function($http){
+app.service('parseService', function($http, $q){
   //Here you'll need to create two methods. One called postData and the other called getData.
   
   //On the lines below create a getData method. This method will retrieve data from the parse backend.
   //The url for the get request should be 'https://api.parse.com/1/classes/chat?order=-createdAt'
   //Be sure to return whatever gets returned from $http so you can call .then in your controller.
- 
 
 
   //On the line below create the postData method. This method will add data to the parse backend.
@@ -16,7 +15,34 @@ app.service('parseService', function($http){
   //Also, remember that $http returns a promise. So if you return the whole $http call (return $http(...)), you can then use .then in your controller.
   
   //postData method here
-  
+  this.postData = function(yourMessage) {
+    var future = $q.defer();
+    $http({
+        method: 'POST',
+        url: 'https://api.parse.com/1/classes/chat',
+        data: {
+            text: yourMessage
+        }
+    }).then(function(res) {
+        future.resolve(res.data);
+    }, function(err) {
+        future.reject(err);
+    });
+    return future.promise;
+  }  
   
   //getData method here
+  this.getData = function() {
+    var future = $q.defer();
+    $http({
+        method: 'GET',
+        url: 'https://api.parse.com/1/classes/chat?order=-createdAt' 
+    }).then(function(res) {
+        future.resolve(res.data);
+    }, function(err) {
+        future.reject(err);
+    });
+    return future.promise;
+  }
+
 });
